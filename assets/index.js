a1398c49b30c5e2c305f2e12b4221070df3334b0
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-04-21 13:04:03 +1000

    questions added

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,54 @@ const inquirer = require('inquirer');
 console.log('gen is running');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'A description of your project?',
+},
+{
+    type: 'input',
+    name: 'contents',
+    message: 'List a table of contents:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Any Installation instructions?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Project Usage?',
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Contribution info?',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What is your License?',
+},
+{
+    type: 'input',
+    name: 'username',
+    message: 'What is your Github username?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+},
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
